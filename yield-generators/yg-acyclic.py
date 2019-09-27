0bd83cbd92e8ff54f57494c0f859e850a2d5b4e7
Daniel Kraft <d@domob.eu>
2019-09-27 18:02:21 +0200

    Introduce yg-acyclic.
    This adds a new custom yield-generator script, yg-acyclic.py.  This works
    very similar to the standard "basic" yield generator, except that it stops
    when funds are in the "lowest" mixdepth rather than sending them back to
    the initial one.
    
    See https://github.com/JoinMarket-Org/joinmarket-clientserver/issues/404
    for some discussion of this.

---
+++ yield-generators/yg-acyclic.py
@@ -0,0 +1,40 @@
+#! /usr/bin/env python
+from __future__ import (absolute_import, division,
+                        print_function, unicode_literals)
+from builtins import * # noqa: F401
+from future.utils import iteritems
+
+from jmbase import jmprint
+from jmclient import YieldGeneratorBasic, ygmain
+
+# User settings
+txfee = 100
+cjfee_a = 500
+cjfee_r = '0.00002'
+ordertype = 'swreloffer'
+nickserv_password = ''
+minsize = 100000
+gaplimit = 6
+
+
+class YieldGeneratorAcyclic(YieldGeneratorBasic):
+    """A yield-generator bot that sends funds linearly through the
+    mixdepths, but not back from the "lowest" depth to the beginning.
+    Instead, it lets funds accumulate there, so that they can then be manually
+    sent elsewhere as needed."""
+
+    def __init__(self, wallet, offerconfig):
+        super(YieldGeneratorAcyclic, self).__init__(wallet, offerconfig)
+
+    def get_available_mixdepths(self):
+        balances = self.wallet.get_balance_by_mixdepth(verbose=False)
+        return {m: b for m, b in iteritems(balances)
+                     if m < self.wallet.mixdepth}
+
+
+if __name__ == "__main__":
+    ygmain(YieldGeneratorAcyclic, txfee=txfee,
+           cjfee_a=cjfee_a, cjfee_r=cjfee_r,
+           ordertype=ordertype, nickserv_password=nickserv_password,
+           minsize=minsize, gaplimit=gaplimit)
+    jmprint('done', "success")
